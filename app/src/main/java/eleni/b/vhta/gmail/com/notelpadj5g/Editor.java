32c5e90bc79315de200af3e2f2350f5eff2d9274
Eleni <eleni.b.vhta@gmail.com>
2018-10-31 20:50:06 +0200

    Initial commit

---
+++ app/src/main/java/eleni/b/vhta/gmail/com/notelpadj5g/Editor.java
@@ -0,0 +1,13 @@
+package eleni.b.vhta.gmail.com.notelpadj5g;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Editor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
---
+++ app/src/main/java/eleni/b/vhta/gmail/com/notelpadj5g/View_Notes.java
@@ -0,0 +1,32 @@
+package eleni.b.vhta.gmail.com.notelpadj5g;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class View_Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view__notes);
+
+        FloatingActionButton newNote= findViewById(R.id.NewNote);
+        newNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(),Editor.class);
+                startActivityForResult(intent,0);
+            }
+        });
+        }
+
+
+
+
+
+    }
+
