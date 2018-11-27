3fa04ee9a262c1985f2697abdc80efab5ba8b73c
smarog <smaro13@windowslive.com>
2018-11-27 22:26:21 +0200

    View Delete Save working final

--- app/src/main/java/eleni/b/vhta/gmail/com/notelpadj5g/View_Notes.java
+++ app/src/main/java/eleni/b/vhta/gmail/com/notelpadj5g/View_Notes.java
@@ -1,21 +1,63 @@
 package eleni.b.vhta.gmail.com.notelpadj5g;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+
 
 public class View_Notes extends AppCompatActivity {
+    ArrayList<String> listItem;
+    ArrayAdapter adapter;
+    ListView list;
+    AlertDialog alertDialog = null;
+    Database db = new Database(this);
+    String item;
+    SwipeRefreshLayout swipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view__notes);
 
-        Database db = new Database(this);
+        db = new Database(this);
+        listItem = new ArrayList<>();
+        list = findViewById(R.id.ListView);
 
+        viewData();
+
+        list.setOnItemClickListener(onListClick);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                (new Handler()).postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        swipeRefreshLayout.setRefreshing(false);
+                        listItem = new ArrayList<>();
+                        list = findViewById(R.id.ListView);
+                        viewData();
+                    }
+                },2000);
+            }
+        });
         FloatingActionButton newNote= findViewById(R.id.NewNote);
         newNote.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,11 +66,75 @@ public class View_Notes extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
-        }
-
-
-
-
 
     }
+
+    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            item= list.getItemAtPosition(position).toString();
+            edit_box();
+        }
+    };
+
+    private void edit_box()
+    {
+        final View deleteView = getLayoutInflater().inflate(R.layout.delete_dialog,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("What do you wish to do?");
+        builder.setView(deleteView);
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton("View Note or Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNeutralButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (item==null)
+                {
+                    return;
+                }
+                else
+                {
+                    db.delete(item);
+                    item = null;
+                    listItem = new ArrayList<>();
+                    list = findViewById(R.id.ListView);
+                    viewData();
+
+                }
+            }
+
+        });
+        builder.show();
+    }
+
+    public void viewData() {
+        Cursor cursor= db.viewData();
+        if (cursor.getCount()==0){
+            Toast.makeText(this,"no data to show", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            while (cursor.moveToNext()){
+                String date=cursor.getString(0);
+                String title=cursor.getString(1);
+                String total = date + " - " + title;
+                listItem.add(total);
+            }
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listItem);
+        list.setAdapter(adapter);
+    }
+}
 
